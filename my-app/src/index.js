c9bd65cb2026008f0c541772385096ffbbee404d
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-02-28 15:26:48 +0530

    TodoieApp

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import "../src/index.css"
 
-const roots = ReactDOM.createRoot(document.getElementById('root'));
-roots.render(<h1>Hello Suraj</h1>);
+function MyComponent(){
+    const link = "https://instagram.com/itsmesuraj20";
+    return <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><a href= {link}>CheckOutMy Instagram</a></button>
+}
 
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(<MyComponent />);
---
+++ todoie-app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () =>{
+  
+  return <button type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><a link="https://instagram.com/itsmesuraj20" >Todoie App</a></button>;
+};
+
+
+export default App;
